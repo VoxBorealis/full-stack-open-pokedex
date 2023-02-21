d2e9c4e032dcbeb3ae4b503076c7fd95fd2c3451
Vox Borealis <uusitalo.y@gmail.com>
2023-02-21 18:16:36 +0200

    update healthcheck to confirm that /health returns 'ok'

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "Hello from shell script"
-
-exit 1 # exit status 1 means that the script "fails"
+response=$(curl http://localhost:5000/health)
+if [ $response == "ok" ];
+then
+exit 0
+else
+exit 1
+fi
